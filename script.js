24af7bb377e06cbf5ba974af0c862ae9e0e08337
Mayara Cosmo <mayara.cosmoce@gmail.com>
2024-02-28 21:53:44 -0300

    novidade

---
+++ script.js
@@ -0,0 +1,57 @@
+// let nome = prompt ("Digite seu nome");
+//let idade = Number (prompt ("Digite sua idade"));
+
+//alert (`Olá, ${nome} ${idade} você está devendo`);
+
+/*
+let cachorro = [1,2,3, "shitzu", "labrador", "pastor alemão", "pé duro", "pinscher", 4]
+
+let racas = cachorro [3]
+
+console.log (racas)
+
+
+let exemplo = [1,3,["Teste","Desse","Array"],[true,2,"Undefined"],5,["Pera","Uva","Maça"],[2,4,6,3]]
+
+// Quero que retornem Teste
+//Quero que retornem 5
+//Quero que retornem Undefined e digam qual o tipo 
+// Quero que retornem Maça
+// Quero que retornem 6
+
+console.log (exemplo[2][0])
+console.log (exemplo[4])
+console.log (exemplo[3][2])
+console.log (exemplo[5][2])
+console.log (exemplo[6][2])
+
+let exemplo = [1,3["teste", "desse", [true,2,"underfined"]], [5, ["bulldog","shitzu"[12,42,60,["chevette", "porshe"]]]]]
+*/
+
+
+let carro = {
+    modelo:"Chevette",
+    ano:"1979",
+    cor:"Marrom bombom",
+    chassi:"DD73H4723DUSIHD"
+}
+
+console.log (`O carro de modelo ${carro.modelo} e de ano ${carro.ano} está na oficina há 20 dias`);
+
+let alunos = {
+    nome:"Mayara",
+    email:"mayara@hdjdjd",
+    matricula:"272272",
+    curso:"Dev"
+}
+
+console.log (`A aluna com a matrícula ${alunos.matricula} está inadiplente`);
+
+
+let objetos = {
+    compraId: "CompraID";
+    nomeobjeto: "mesa";
+    quantidade: 1;
+    valor: 50;
+
+}
